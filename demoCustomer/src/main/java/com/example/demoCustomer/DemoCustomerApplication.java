28a25f3f1adc479d548b7d76891466e9fdf78f11
shinde-shantanu <shantanushinde.shinde@gmail.com>
2023-05-25 21:13:31 -0400

    Initial commit

---
+++ demoCustomer/src/main/java/com/example/demoCustomer/DemoCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoCustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCustomerApplication.class, args);
+	}
+
+}
---
+++ demoCustomer/src/test/java/com/example/demoCustomer/DemoCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
